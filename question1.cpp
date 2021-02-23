6b53f2a72527670ba3fbc8fc60d4b522afb0f407
dsoft-hoanghn <hoanghn@d-soft.com.vn>
2021-02-23 22:13:11 +0700

    add file

---
+++ question1.cpp
@@ -0,0 +1,69 @@
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+class Node
+{
+public:
+    Node(int value, Node* left, Node* right)
+    {
+        this->value = value;
+        this->left = left;
+        this->right = right;
+    }
+
+    int getValue() const
+    {
+        return value;
+    }
+
+    Node* getLeft() const
+    {
+        return left;
+    }
+
+    Node* getRight() const
+    {
+        return right;
+    }
+
+private:
+    int value;
+    Node* left;
+    Node* right;
+};
+
+class BinarySearchTree
+{
+    public:
+        static bool contains(const Node& root, int value)
+        {
+            if(value == root.getValue()) return true;
+            else
+            {
+                if(value > root.getValue()) return contains(*root.getRight(),value);
+                else
+                {
+                    if (value < root.getValue()) return contains(*root.getLeft(),value);
+                    else
+                    {
+                        return false;
+                    }
+                }   
+            }
+            
+            throw std::logic_error("Waiting to be implemented");
+        }
+       
+};
+
+#ifndef RunTests
+int main()
+{
+    Node n1(1, NULL, NULL);
+    Node n3(3, NULL, NULL);
+    Node n2(2, &n1, &n3);
+
+    std::cout << "this is result: " << BinarySearchTree::contains(n2, 9);
+}
+#endif
